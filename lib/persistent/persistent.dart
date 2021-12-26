700ff9df9885a3c201020fdd3445e6c2174467c3
alisahin07 <alisahin172@gmail.com>
2021-12-26 14:39:40 +0300

    yeah this is first commit

---
+++ lib/persistent/persistent.dart
@@ -0,0 +1,25 @@
+class Persistent {
+  static List<String> jobsList = [
+    'Mobile Developer',
+    'Game Developer',
+    'Web Designer',
+    'HR Manager',
+    'Manager',
+    'Team Leader',
+    'Designer',
+    'Full stack developer',
+    'marketing',
+    'Digital Marketing',
+  ];
+  static List<String> jobCategoryList = [
+    'Architecture and Construction',
+    'Education and Traning',
+    'Development - Programming',
+    'Business',
+    'Information Technology',
+    'Human resource',
+    'Marketing',
+    'Design',
+    'Accounting'
+  ];
+}
