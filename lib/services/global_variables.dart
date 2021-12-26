6ad162f7bd450c4c5540cf2e045a5d5d35cc07eb
alisahin07 <alisahin172@gmail.com>
2021-12-26 15:07:01 +0300

    yeah t

--- lib/services/global_variables.dart
+++ lib/services/global_variables.dart
@@ -1,7 +1,8 @@
-
-
 String name = "";
 String userImage = "";
 String location = "";
-String loginUrlImage = "https://costar.brightspotcdn.com/71/cf/3fda9cc54559af417803fdb67753/gettyimages-1169688104-1.jpg";
-String signUpUrlImage = "https://upload.wikimedia.org/wikipedia/commons/f/f7/LinkedIn_Headquarters_Mountain_View.jpg";
+String loginUrlImage =
+    "https://costar.brightspotcdn.com/71/cf/3fda9cc54559af417803fdb67753/gettyimages-1169688104-1.jpg"; //burasi ana ekranda animasyon olan yerler
+String signUpUrlImage =
+    "https://upload.wikimedia.org/wikipedia/commons/f/f7/LinkedIn_Headquarters_Mountain_View.jpg"; //burasi ana ekranda animasyon olan yerler
+String deli = "";
